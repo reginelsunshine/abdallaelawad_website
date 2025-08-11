6d05e9bb52682f1d9f372d06d893e0156f3e91ea
abdallaelawad <63597236+reginelsunshine@users.noreply.github.com>
2025-08-11 18:23:05 +0200

    stile modificato e immagine aggiunta
    aggiunte anche due cards

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    "./src/**/*.{html,js}",  // oppure il percorso corretto per i tuoi file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
